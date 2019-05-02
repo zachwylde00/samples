47b92621228b7bbc728a9e1a7dc6c8b15acd92e9
kavixooa <kavi.sarna@xooa.com>
2019-05-02 19:07:05 +0530

    Swagger and Readme

--- erc20/README.md
+++
@@ -1,4 +0,0 @@
-this contract takes 3 input arguments 
-```
-["symbol", "name", "tokenSupply"]
-```
--- erc20/smartcontract/vendor/github.com/s7techlab/cckit/router/handler.go
+++
@@ -1,5 +0,0 @@
-package router
-
-func EmptyContextHandler(c Context) (interface{}, error) {
-	return nil, nil
-}
